6cfb0abe3bd73c2d9ce96fef6c35fcc2801df764
Steven <hellosteventran@gmail.com>
2020-06-03 02:07:56 +0000

    Added added to cart Modal

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AddToCartModal from './addToCartModal.jsx';
 
 export default class ProductDetails extends React.Component {
   constructor(props) {
@@ -28,31 +29,48 @@ export default class ProductDetails extends React.Component {
   }
 
   render() {
-
+    let addedToCartModal = null;
+    if (this.state.product === null) {
+      return (
+        <div className="descriptionContainer pt-5 justify-content-center">
+          <div className="descriptionBackground d-flex bg-white p-5 container">
+          </div>
+        </div>
+      );
+    }
+    if (this.props.addedToCart === true) {
+      addedToCartModal = <AddToCartModal name={this.state.product.name} setViewFunction={this.props.setViewFunction} handleAddToCartFunction={this.props.handleAddToCartFunction} />;
+    } else if (this.props.addedToCart === false) {
+      addedToCartModal = null;
+    }
     const product = this.state.product;
     if (this.state.product === null) {
       return (
         null
       );
+
     } else {
       return (
-        <div className="descriptionContainer pt-5 justify-content-center">
-          <div className="descriptionBackground d-flex bg-white p-5 container">
-            <div className="backToCatalog" onClick={() => this.props.setViewFunction('catalog', {})}><i className="far fa-times-circle back"></i></div>
-            <div className="row w-100">
-              <div className="col-md-7">
-                <div className="descriptionImageBox">
-                  <img className="descriptionImage" alt="An image of the product" title="product image" src={product.imageDetail} />
+        <div>
+          {addedToCartModal}
+          <div className="descriptionContainer pt-5 justify-content-center">
+            <div className="descriptionBackground d-flex bg-white p-5 container">
+              <div className="backToCatalog" onClick={() => this.props.setViewFunction('catalog', {})}><i className="far fa-times-circle back"></i></div>
+              <div className="row w-100">
+                <div className="col-md-7">
+                  <div className="descriptionImageBox">
+                    <img className="descriptionImage" alt="An image of the product" title="product image" src={product.imageDetail} />
+                  </div>
                 </div>
-              </div>
-              <div className="productDetails col-md-5">
-                <div>
-                  <h2 className="descriptionName">{product.name}</h2>
-                  <div className="descriptionDivider"></div>
-                  <h4 className="descriptionProductPrice"><strong>${((product.price) / 100).toFixed(2)}</strong></h4>
-                  <h6 className="text-muted">Material: <em>{product.madewith}</em></h6>
-                  <p className="descriptionShortDescription">{product.shortDescription}</p>
-                  <button onClick={() => this.props.addToCartFunction(product)} className="addToCartButton">Add to Cart</button><br/>
+                <div className="productDetails col-md-5">
+                  <div>
+                    <h2 className="descriptionName">{product.name}</h2>
+                    <div className="descriptionDivider"></div>
+                    <h4 className="descriptionProductPrice"><strong>${((product.price) / 100).toFixed(2)}</strong></h4>
+                    <h6 className="text-muted">Material: <em>{product.madewith}</em></h6>
+                    <p className="descriptionShortDescription">{product.shortDescription}</p>
+                    <button onClick={() => this.props.addToCartFunction(product)} className="addToCartButton">Add to Cart</button><br/>
+                  </div>
                 </div>
               </div>
             </div>
